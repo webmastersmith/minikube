6d12e5af1a18b44ac186ac70b636b36a1a9788d1
Bryon Smith <webmastersmith37@gmail.com>
2022-08-07 16:57:53 -0500

    minor change

---
+++ node/hello/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const os = require('os')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(`Hello World from ${os.hostname}`)
+})
+
+app.get('/nginx', (req, res) => {
+  const url = 'http://nginx'
+  fetch(url).then((response) => {
+    return response.text()
+  }).then((text) => {
+    res.send(text)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
